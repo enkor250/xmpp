01f1b9795b0c4051a8e5097ab9195b4c42531e00
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-11-23 10:54:48 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,25 @@
+apt install git mc  -y
+
+MYIP=$(curl -4 https://icanhazip.com/);
+
+#docker
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+apt install git mc  -y
+
+#certbot
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
+
+cd /opt
+git clone https://github.com/it-toppp/xmpp.git
+chown 999 -R /opt/xmpp/ejabberd
+
+echo "open in web browser    :  https://$MYIP:5280/admin"
