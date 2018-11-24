7181b8c548e20bcdba1b358a7ed8aa19a4e18e04
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-11-24 15:30:11 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,10 @@
 echo "Enter domain: "
 read DOMAIN
 
-apt install git mc -y
+echo "Enter admin password: "
+read ADMINPASS
+
+apt install curl git mc -y
 
 MYIP=$(curl -4 https://icanhazip.com/);
 
@@ -15,12 +18,12 @@ chmod +x /usr/local/bin/docker-compose
 #certbot
 sudo apt-get update
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:certbot/certbot
+sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt-get update
 sudo apt-get install certbot -y
 
 certbot register --email admin@$DOMAIN --agree-tos
-certbot certonly --standalone -m admin@$DOMAIN --agree-tos -d $DOMAIN --rsa-key-size 4096 --server https://acme-v02.api.letsencrypt.org/directory
+certbot certonly --standalone -n -m admin@$DOMAIN --agree-tos -d $DOMAIN --rsa-key-size 4096 --server https://acme-v02.api.letsencrypt.org/directory
 
 cd /opt
 git clone https://github.com/it-toppp/xmpp.git
@@ -33,25 +36,24 @@ sudo docker run -u root \
     --privileged \
     --detach \
     --restart=always \
-#    --net=host \
-    -p 5222:5222 \
+    -p 5221:5221 \
     -p 5269:5269 \
     -p 5280:5280 \
     -p 5277:5277 \
-    -p 5443:5443 \
+    -p 5444:5444 \
     -e "XMPP_DOMAIN=$DOMAIN" \
     -e "EJABBERD_ADMINS=admin@$DOMAIN" \
-    -e "EJABBERD_USERS=admin@e$DOMAIN:123456" \
+    -e "EJABBERD_USERS=admin@$DOMAIN:ADMINPASS" \
     -e "EJABBERD_REGISTER_ADMIN_ONLY=true" \
-  #  -e "TZ=Europe/Berlin" \
-    -v /var/lib/openvpn/mongodb:/var/lib/mongodb \
     -v /opt/xmpp/ejabberd/database/:/opt/ejabberd/database \
     -v /opt/xmpp/ejabberd/conf/:/opt/ejabberd/conf \
     -v /opt/xmpp/ejabberd/ssl/:/opt/ejabberd/ssl \
     -v /opt/xmpp/ejabberd/upload/:/opt/ejabberd/upload \
        rroemhild/ejabberd
        
-     cat /etc/letsencrypt/live/$DOMAIN/{privkey,fullchain}.pem > /opt/xmpp/ejabberd/ssl/$DOMAIN.pem
-     docker restart ejabberd
+ cat /etc/letsencrypt/live/$DOMAIN/{privkey,fullchain}.pem > /opt/xmpp/ejabberd/ssl/$DOMAIN.pem
+ docker restart ejabberd
      
 echo "open in web browser    :  https://$DOMAIN:5280/admin"
+echo "user : admin    ;  password    :  $ADMINPAS"
+
